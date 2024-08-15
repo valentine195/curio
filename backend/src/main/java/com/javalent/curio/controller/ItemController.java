d85277b3d01701b07803c77ace0da471fff3f4a2
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2024-08-15 00:06:49 -0400

    fix: Adds /api

--- backend/src/main/java/com/javalent/curio/controller/ItemController.java
+++ backend/src/main/java/com/javalent/curio/controller/ItemController.java
@@ -11,8 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api")
 public class ItemController {
     @Autowired
     private ItemRepository items;
--- backend/src/main/java/com/javalent/curio/controller/MuseumController.java
+++ backend/src/main/java/com/javalent/curio/controller/MuseumController.java
@@ -9,9 +9,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
+@RequestMapping("/api")
 public class MuseumController {
     @Autowired
     private MuseumRepository museums;
