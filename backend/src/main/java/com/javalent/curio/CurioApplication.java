96c6e6a88065703bdc1e1e8d3eae8a0a5028e2fd
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2024-08-21 17:08:54 -0400

    fix: Adds injected CORS config

--- backend/src/main/java/com/javalent/curio/CurioApplication.java
+++ backend/src/main/java/com/javalent/curio/CurioApplication.java
@@ -1,13 +1,33 @@
 package com.javalent.curio;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CurioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CurioApplication.class, args);
+    }
+
+    @Value("#{'${cors.origins}'.toLowerCase().split(',')}")
+    private List<String> origins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins(
+                        origins.toArray(new String[0]));
+            }
+        };
     }
 
 }
