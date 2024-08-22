bdf95e76d32b820b52acf67436191096ff55df90
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2024-08-21 20:49:01 -0400

    fix: arghhhh

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,7 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { dev } from "$app/environment";
-import type { ItemResponse } from "../routes/items/types";
-const BASE_URL = dev ? "http://localhost:8080" : window.location.origin;
+const BASE_URL = dev ? "http://localhost:8080" : "https://curio.javalent.com";
 export async function fetchItems(page = 0, perPage = 50) {
     return await (
         await fetch(`${BASE_URL}/api/items?page=${page}&pageSize=${perPage}`)
